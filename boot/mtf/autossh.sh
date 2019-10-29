07f726ae43940322f1fd42ee133d7730b3411b23
Jason Sherman <jsnshrmn@users.noreply.github.com>
2019-10-29 19:38:30 +0000

    not every command expands ~ to home dir

--- boot/mtf/autossh.sh
+++ boot/mtf/autossh.sh
@@ -19,11 +19,11 @@ EOF
 
 # stage remote ssh config
 mkdir -p ~/.remotesshconfig
-echo "$MTFREMOTESSHCONFIG"| tee "~/.remotesshconfig/${hostname}" >/dev/null
+echo "$MTFREMOTESSHCONFIG"| tee "/home/pi/.remotesshconfig/${hostname}" >/dev/null
 
 # send remote ssh config
 /usr/bin/ssh mtf_ssh_server 'mkdir -p ~/.ssh/config.d && echo "Include config.d/*">~/.ssh/config'
-scp "~/.remotesshconfig/${hostname}" mtf_ssh_server:~/.ssh/config.d/${hostname}
+scp "/home/pi/.remotesshconfig/${hostname}" mtf_ssh_server:~/.ssh/config.d/${hostname}
 
 # start tunnel
 /usr/bin/ssh -NT -o ExitOnForwardFailure=yes -o ServerAliveInterval=60 -o ServerAliveCountMax=3 mtf_ssh_tunnel
